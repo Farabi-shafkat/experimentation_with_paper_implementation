72e9011d1b80e6c22a260d5ce254027800bc2605
random_user <farabi.shafkat@gmail.com>
2020-06-07 00:27:50 +0600

    custom c3d

---
+++ models/C3D_flat1.py
@@ -0,0 +1,117 @@
+# Author: Paritosh Parmar (https://github.com/ParitoshParmar)
+#
+# Code for C3D-LSTM used in:
+# [1] @inproceedings{parmar2017learning,
+#   title={Learning to score olympic events},
+#   author={Parmar, Paritosh and Morris, Brendan Tran},
+#   booktitle={Computer Vision and Pattern Recognition Workshops (CVPRW), 2017 IEEE Conference on},
+#   pages={76--84},
+#   year={2017},
+#   organization={IEEE}}
+#
+# [2] @article{parmar2018action,
+#   title={Action Quality Assessment Across Multiple Actions},
+#   author={Parmar, Paritosh and Morris, Brendan Tran},
+#   journal={arXiv preprint arXiv:1812.06367},
+#   year={2018}}
+
+import torch
+import torch.nn as nn
+#from opts import *
+import torch.nn.functional as F
+class C3D(nn.Module):
+    """
+    The C3D network as described in [1].
+    """
+
+    def __init__(self):
+        super(C3D, self).__init__()
+
+        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv5a = nn.Conv3d(512, 512, kernel_size=(1, 3, 3), padding=(0, 1, 1))
+        self.conv5b = nn.Conv3d(512, 512, kernel_size=(1, 3, 3), padding=(0, 1, 1))
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+        
+
+       # self.fc6 = nn.Linear(8192, 4096)
+        # self.fc7 = nn.Linear(4096, 4096)
+        # self.fc8 = nn.Linear(4096, 487)
+
+       # self.dropout = nn.Dropout(p=0.2)
+
+        self.relu = nn.ReLU()
+        # self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+
+        h = self.relu(self.conv1(x))
+        h = self.pool1(h)
+
+        h = self.relu(self.conv2(h))
+        h = self.pool2(h)
+
+        h = self.relu(self.conv3a(h))
+        h = self.relu(self.conv3b(h))
+        h = self.pool3(h)
+
+        h = self.relu(self.conv4a(h))
+        h = self.relu(self.conv4b(h))
+        h = self.pool4(h)
+
+        h = self.relu((self.conv5a(h)))
+        h = self.relu((self.conv5b(h)))
+        h = self.pool5(h)
+        #torch.flatten(h)
+
+       
+
+        
+       
+        h = h.view(-1, 8192)
+
+        
+
+       # print('Shape of pool5: ', h1.shape)
+       # print('Shape of pool51: ', h.shape)
+       # if torch.all(h1.eq(h)):
+       #     print("it makes no sense")
+       # else:
+       #     print("diiferent")
+       # h = self.relu(self.fc6(h))
+        # h = self.dropout(h)
+        # h = self.relu(self.fc7(h))
+        # h = self.dropout(h)
+
+        # logits = self.fc8(h)
+        # probs = self.softmax(logits)
+      #  h= F.relu(h)
+        return h1
+
+
+if __name__ == "__main__":
+    c3d = C3D()
+    dummy_clip =  torch.zeros(1,3, 16, 112, 112)
+    print(dummy_clip.shape)
+    h = c3d(dummy_clip)
+
+
+
+"""
+References
+----------
+[1] Tran, Du, et al. "Learning spatiotemporal features with 3d convolutional networks." 
+Proceedings of the IEEE international conference on computer vision. 2015.
+"""
